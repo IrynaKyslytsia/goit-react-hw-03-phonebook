20cf72a93c8ee0e2f7bd3b2d5d29179ec6e44d5d
IrynaKyslytsia <i.kislica89@gmail.com>
2023-05-11 23:10:15 +0300

    Added save to Local Storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,19 +8,34 @@ import { Container, MainTitle, SecondTitle } from 'components/App/App.styled'
 class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter: ''
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    };
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    };
   };
 
   addContact = (data) => {
     const { contacts } = this.state;
     const newContact = {
-      ...data,
-      id: nanoid()
+      id: nanoid(),
+      ...data
     };    
 
     if (contacts.find(contact => contact.name === newContact.name)) {
